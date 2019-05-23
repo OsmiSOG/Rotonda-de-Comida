35ec9c86b76d2fd2ba191b5eb7e533a71ed8038f
Manuels Garcia <mgarciamons@gmail.com>
2019-05-23 00:12:50 -0500

    corrigiendo errores en las rutas

--- model/DAO/IngredientManagement.php
+++ model/DAO/IngredientManagement.php
@@ -1,9 +1,7 @@
 <?php
-require_once $_SERVER['Rotonda-de-comida/model/interface/interfaceIngredient.php'];
 require_once $_SERVER['DOCUMENT_ROOT'].'/Rotonda-de-Comida/model/dataSource/Connection.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/Rotonda-de-Comida/model/interface/interfaceIngredient.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/Rotonda-de-Comida/model/transferObject/ingredient.php';
-
 
 /**
  *
