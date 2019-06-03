d0d6a2aeeae68fab0bb17fa758db5d513dcd9fe6
Manuels Garcia <mgarciamons@gmail.com>
2019-06-03 15:29:10 -0500

    consultas de orden

--- controller/restaurant/home.php
+++ controller/restaurant/home.php
@@ -3,13 +3,13 @@ session_start();
 include_once '../../model/DAO/MenuManagement.php';
 include_once '../../model/transferObject/Menu.php';
 include_once '../../model/transferObject/Product.php';
-include_once '../../model/DAO/RestaurantManagement,php';
+include_once '../../model/DAO/RestaurantManagement.php';
 include_once '../../model/DAO/ProductManagement.php';
 include_once '../../model/transferObject/Restaurant.php';
 $menus = array();
 if (isset($_SESSION['restaurant'])) {
 $menuDAO = new MenuManagement();
-$menus = $menuDAO -> getRestaurantMenus();
+$menus = $menuDAO -> getRestaurantMenus($_SESSION['restaurant']);
 } else  {
   header('location: ../index.php');
 }
