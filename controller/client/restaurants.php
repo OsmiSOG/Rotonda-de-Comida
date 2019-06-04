bdea8e0ddafe8535114525f4c795a6c3707c1811
Andres Fiscal <agarciafisc@uniminuto.edu.co>
2019-06-04 13:15:52 -0500

    actualizacion

--- controller/client/restaurants.php
+++ controller/client/restaurants.php
@@ -3,13 +3,13 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 session_start();
-include_once '../../model/DAO/ClientManagement.php'; // incluir dao
+include_once '../../model/DAO/RestaurantManagement.php'; // incluir dao
 include_once '../../model/transferObject/Restaurant.php';
 $restaurants = array();
 if (isset($_SESSION['client'])) {
-  $restaurantDAO = new ClientManagement();
+  $restaurantDAO = new RestaurantManagement();
   $restaurant = new Restaurant();
-  $Allrestaurants = $restaurantDAO -> getRestaurants($nit);
+  $Allrestaurants = $restaurantDAO -> getRestaurants();
 } else  {
   header('location: ../index.php');
 }
